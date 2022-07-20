9d4bfebbe5368c0640baad58e44ca3d4fc24d8b8
mattyo7126 <hyacinth.126@icloud.com>
2022-07-20 11:34:47 +0900

    install sass

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import axios from '../axios';
+import './Row.scss';
+
+const base_url = 'https://image.tmdb.org/t/p/original';
 
 type Props = {
   title: string;
@@ -16,7 +19,7 @@ type Movie = {
   backdrop_path: string;
 };
 
-export const Row = ({ title, fetchUrl }: Props) => {
+export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
@@ -30,5 +33,22 @@ export const Row = ({ title, fetchUrl }: Props) => {
 
   console.log(movies);
 
-  return <div className="Row">{title}</div>;
+  return (
+    <div className="Row">
+      <h2>{title}</h2>
+      <div className="Row-posters">
+        {/* ポスターコンテンツ */}
+        {movies.map((movie, i) => (
+          <img
+            key={movie.id}
+            className={`Row-poster ${isLargeRow && 'Row-poster-large'}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
