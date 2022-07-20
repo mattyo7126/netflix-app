2d0f6b5e16bf6b32720ed1dfb56357421356a92b
mattyo7126 <hyacinth.126@icloud.com>
2022-07-20 17:27:29 +0900

    create Banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Row } from './components/Row';
+import { Banner } from './components/Banner';
 import { requests } from './request';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.feachNetflixOriginals}
