144aa37f746e51b72e97ef0121789ad75581c896
mattyo7126 <hyacinth.126@icloud.com>
2022-07-20 21:01:34 +0900

    create Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Row } from './components/Row';
+import { Nav } from './components/Nav';
 import { Banner } from './components/Banner';
+import { Row } from './components/Row';
 import { requests } from './request';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
